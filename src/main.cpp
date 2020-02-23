d6d1ff3b4a25fb0f116d1f4f79953b79b3bd9eba
Kendall Goto <k@kgo.to>
2020-02-23 17:51:38 -0500

    completed codebase

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,28 @@
 //define the states in easy-to-read terms
 #include <Arduino.h>
 #include <iostream>
-using namespace std;
+#include <ESP32Servo.h>
 enum {ALARM_OFF, ALARM_ARMED, ALARM_INTRUDER};
 int currentState = ALARM_OFF; //start off with the alarm deactivated
 
 void HandleArmingButton();
-void handleLaserBroken();
-void handleDisarmingButton();
+void HandleLaserBroken();
+void HandleDisarmingButton();
 bool CheckArmingButton();
 bool CheckIfLaserBroken();
 bool CheckDisarmingButton();
 
 //Some pin definitions:
-const int buttonArmingPin       = 32;
-const int buttonDisarmingPin    = 33;
+const int buttonArmingPin       = 33;
+const int buttonDisarmingPin    = 32;
 const int photoPin	            = 27;
 const int piezoPin	            = 26;
 const int ledPin	            = 14;
-const int servoPin	            = 25;
+const int servoPin	            = 12;
 
-void setup(void)
+Servo myservo;  // create servo object to control a servo
+
+void setup()
 {
 	Serial.begin(115200);
 	Serial.println("Hello!");
@@ -31,24 +33,21 @@ void setup(void)
 	pinMode(piezoPin, OUTPUT);
 	pinMode(ledPin, OUTPUT);
 	pinMode(servoPin, OUTPUT);
-
+    myservo.attach(servoPin, 750, 2250);
 	delay(500);
 
 	Serial.println("setup() complete");
 }
 
-void loop(void)
-/*
- * Our loop() is just a set of checker-handler pairs.
- */
+void loop()
 {
-		//output ADC value
+    Serial.println("Loop");
 	if(CheckArmingButton()) HandleArmingButton();
 	if(CheckIfLaserBroken()) HandleLaserBroken();
 	if(CheckDisarmingButton()) HandleDisarmingButton();
 }
 
-void HandleArmingButton(void)
+void HandleArmingButton()
 {
 	if(currentState == ALARM_OFF)
 	{
@@ -57,27 +56,39 @@ void HandleArmingButton(void)
 		//take action: light the LED
 		digitalWrite(ledPin, HIGH);
 
-		//take action: you get to ADD CODE HERE to move the servo!
-		
-		currentState = ALARM_ARMED; //Don't forget to change the state!
+		//TODO: spin motor
+		myservo.write(135);
+		currentState = ALARM_ARMED;
 	}
 }
 
-void HandleLaserBroken(void)
+void HandleLaserBroken()
 {
 	//YOUR CODE HERE!
-	
+	if(currentState == ALARM_ARMED) {
+        Serial.println("Laser Broken!");
+        currentState = ALARM_INTRUDER;
+        tone(piezoPin, 4186);
+    }
+
 }
 
-void HandleDisarmingButton(void)
+void HandleDisarmingButton()
 {
 	//YOUR CODE HERE!
+	if(currentState == ALARM_ARMED || currentState == ALARM_INTRUDER) {
+        Serial.println("Disarmed!");
+        currentState = ALARM_OFF;
+        digitalWrite(ledPin, LOW);
+        myservo.write(1);
+        noTone(piezoPin);
+    }
 }
 
 /*
  * Code for checking the button -- you don't need to edit this one
  */
-bool CheckArmingButton(void)
+bool CheckArmingButton()
 {
 	static int prevButtonState = HIGH; //button up => pin reads HIGH
 
@@ -94,18 +105,20 @@ bool CheckArmingButton(void)
 	return retVal;
 }
 
-/*
- * Code for checking the laser. YOU WILL NEED TO EDIT THIS ONE
- */
 enum {DARK, LIGHT};
-
-bool CheckIfLaserBroken(void)
+bool CheckIfLaserBroken()
 {
 	static int prevLaserSensorState = DARK; //the program starts with the laser off
 	bool retVal = false;
 
 	//add code here to detect the EVENT of the laser being broken. See the button checker above for hints
-
+	Serial.println(analogRead(photoPin));
+    int currentLaserState = (analogRead(photoPin) < 1730) ? LIGHT : DARK;
+    if(currentLaserState != prevLaserSensorState) {
+        if(currentLaserState == DARK)
+            retVal = true;
+    }
+    prevLaserSensorState = currentLaserState;
 	return retVal;
 }
 
@@ -113,9 +126,19 @@ bool CheckIfLaserBroken(void)
  * You'll have to make your own disarming checker!
  */
 
-bool CheckDisarmingButton(void)
+bool CheckDisarmingButton()
 {
-	bool retVal = false;
+    static int prevDisarmState = HIGH; //button up => pin reads HIGH
 
-	return retVal;
+    bool retVal = false;
+
+    int currButtonState = digitalRead(buttonDisarmingPin);
+    if(prevDisarmState != currButtonState)
+    {
+        delay(10); //debounce
+        if(currButtonState == LOW) retVal = true;	//button is down => pin reads LOW
+    }
+    prevDisarmState = currButtonState;
+
+    return retVal;
 }
